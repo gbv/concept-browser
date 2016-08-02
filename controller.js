7beafd9879252c81419918ab7faa2a94b2171446
Jakob Voss <voss@gbv.de>
2016-08-02 15:01:40 +0200

    Move list if schemes to JSON file

--- controller.js
+++ controller.js
@@ -3,32 +3,32 @@
  */
 var browse = angular.module('Browser', ['ngSKOS', 'ui.bootstrap']);
 
+// configuration
 browse.constant('CONFIG', {
-    baseURL: 'http://lvh.me:8080/'
+    baseURL: 'http://localhost:8080/'
 });
 
+// initalization
 browse.run(['$rootScope','$http','CONFIG',function($rootScope,$http,CONFIG) {
-    $rootScope.baseURL = CONFIG.baseURL;
-    $http.get(CONFIG.baseURL).success(function(data){
-        $rootScope.database = data;
-    });
 }]);
 
 browse.controller('conceptBrowserController', ['$scope','$http','$q','CONFIG', function ($scope, $http, $q, CONFIG){
-  $scope.schemes = [ 
-    { prefLabel: { de:"Gemeinsame Normdatei"}, bartoc: "http://bartoc.org/en/node/430", url:"http://www.dnb.de/DE/Standardisierung/GND/gnd_node.html", example:"http://d-nb.info/gnd/4189143-0", php: "GND.php"},
-    { prefLabel: { en:"Wikidata"}, bartoc: "http://bartoc.org/en/node/1940", url:"https://www.wikidata.org/", example:"http://www.wikidata.org/entity/Q36578", php: "Wikidata.php"},
-    { prefLabel: { en:"Iconclass"}, bartoc: "http://bartoc.org/en/node/459", url:"http://www.iconclass.nl/home", example:"http://iconclass.org/25F23(LION)(%2B46)", php:"Iconclass.php"},
-    { prefLabel: { en:"Melvil Decimal Classification"}, bartoc:"http://bartoc.org/en/node/2040", url:"https://www.librarything.com/mds/", example:"http://www.librarything.com/mds/569.8", php: "MDS.php"},
-    { prefLabel: { en:"VIAF"}, url:"http://www.viaf.org/", example:"http://viaf.org/viaf/87772061", php:"VIAF.php"},
-    { prefLabel: { en:"Europeana OpenSKOS API"}, example:"http://id.loc.gov/authorities/subjects/sh2007003224", url:"http://skos.europeana.eu/api", php: "OpenSKOS.php"},
-    { prefLabel: { en:"Geonames"}, example:"http://sws.geonames.org/2918632/", url:"http://www.geonames.org/", bartoc:"http://bartoc.org/en/node/1674", php:"Geonames.php"},
-    { prefLabel: { de:"BARTOC"}, example:"http://bartoc.org/en/node/109", url:"http://bartoc.org/", php:"BARTOC.php"}
-  ];
-  $scope.activeScheme = $scope.schemes[3];
-  $scope.activeURI = $scope.schemes[3].example;
+
+    $scope.loading = true;
+    $scope.schemes = []; 
+    $http.get('schemes.json').then(function(response){
+        $scope.schemes = response.data;      
+        
+        $scope.baseURL = CONFIG.baseURL; //?
+        $scope.selectScheme($scope.schemes[0]);
+    }, function(response) {
+        // TODO: show error
+        $scope.loading = false;
+    }).finally(function() {
+        $scope.loading = false;
+    });
+
   $scope.activeConcept = null;
-  $scope.loading = false;
   $scope.status = {
     isopen: false
   };
@@ -122,7 +122,6 @@ browse.controller('conceptBrowserController', ['$scope','$http','$q','CONFIG', f
 }]);
 
 browse.config(['$httpProvider', function($httpProvider) {
-
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
